04a49fe0818710c8a46a45e3c74fc8572abb6284
Mahmoud-raafat-ac <dodo2_20002002@yahoo.com>
2023-05-14 13:29:55 +0300

    new data

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function (req, res, next) {
   res.render('Home');
 });
 
+/* GET redirect Links. */
+router.get('/ambassadors', function (req, res, next) {
+  res.redirect("https://docs.google.com/forms/d/e/1FAIpQLScZEtFSbrLPzIZcgkUvBa08Ws17huoK9zCzIQsbDd7_8jPUTA/viewform");
+});
+
 module.exports = router;
