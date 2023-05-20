3919c28bee063ccb6b5bcfec79133d1e899896ca
Mahmoud-raafat-ac <dodo2_20002002@yahoo.com>
2023-05-20 14:34:16 +0300

    newData

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,17 @@ router.get('/', function (req, res, next) {
   res.render('Home');
 });
 
-/* GET redirect Links. */
+/* Redirect Links. */
 router.get('/ambassadors', function (req, res, next) {
   res.redirect("https://docs.google.com/forms/d/e/1FAIpQLScZEtFSbrLPzIZcgkUvBa08Ws17huoK9zCzIQsbDd7_8jPUTA/viewform");
+});
+
+router.get('/registration', function (req, res, next) {
+  res.redirect("https://docs.google.com/forms/d/e/1FAIpQLSfxKXTp6wtp_uhpIHEcn_pLId-qOx8G7kKqSxi77u613Cy1fQ/viewform?usp=sf_link");
+});
+
+router.get('/vip', function (req, res, next) {
+  res.redirect("https://docs.google.com/forms/d/e/1FAIpQLSdetuRB4Md8AYItNf9ORjQwE2nYmYrEJ70E-6sXkI75GgxHhw/viewform?usp=sf_link");
 });
 
 module.exports = router;
